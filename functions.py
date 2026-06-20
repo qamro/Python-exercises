62117bb011c15148bef42710535ff697cd3a0aec
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-20 08:03:15 +0200

    functions

--- functions.py
+++ functions.py
@@ -17,3 +17,17 @@ def power(x, y):
 print(f"The power of 2 to the 3 is : {power(2,3)}")
 print(f"The power of 3 to the 4 is : {power(3,4)}")
 
+# factorial function
+
+def factorial(n):
+    if n == 0:
+        return 1
+    else:
+        result = 1
+        for i in range(1, n + 1):
+            result = result * i
+        
+    return result
+
+print(f"The factorial of 5 is : {factorial(5)}")
+print(f"The factorial of 7 is : {factorial(7)}")
