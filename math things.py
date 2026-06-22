c88c3ea07da052b35ce645d5dc2f2d0011357f5a
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-22 04:39:00 +0200

    exercises

--- math things.py
+++ math things.py
@@ -22,3 +22,4 @@ a = float(input("Enter the length of side a: "))
 b = float(input("Enter the length of side b: "))
 c = math.sqrt(math.pow(a, 2) + math.pow(b, 2))
 print(f"The length of side c is: {c}")
+
---
+++ string.py
@@ -0,0 +1,55 @@
+name = input("Enter your full name: ")
+
+print(f"Hello, {name}!")
+
+# string manipulation
+
+
+# count the number of words in the string (The split() method splits the string into a list of words based on whitespace by default.)
+word_count = len(name.split())
+print(f"Number of words in name: {word_count}")
+
+#capitalize the first letter of your string
+capitalized_name = name.capitalize()
+print(f"Capitalized name: {capitalized_name}")
+# convert the string to uppercase
+uppercase_name = name.upper()
+print(f"Uppercase name: {uppercase_name}")
+# convert the string to lowercase
+lowercase_name = name.lower()
+print(f"Lowercase name: {lowercase_name}")
+# count the number of characters in the string or the length of the string  
+name_length = len(name)   
+print(f"Length of name: {name_length}") 
+# count the number of words in the string       
+word_count = len(name.split())
+print(f"Number of words in name: {word_count}")
+# reverse the string (this syntax [::-1] is called slicing and it is used to reverse string or list or set or tuple and the -1 it is the step of the reversing and we can skip some characters by changing the step to -2 or -3 or -4 etc)   
+reversed_name = name[::-1]
+print(f"Reversed name: {reversed_name}")    
+# find a character in the string 
+character = input("Enter a character to find: ")
+if character in name:
+    print(f"Character '{character}' found in name.")
+else:
+    print(f"Character '{character}' not found in name.")
+    
+# find the index of a character in the string  (generally the index of a character is the position of the character in the string and it starts from 0 and it is used to find the position of a character in a string and when we get -1 it means the character is not found in the string) 
+character = input("Enter a character to find its index: ")
+index = name.find(character)
+if index != -1:
+    print(f"Character '{character}' found at index {index}.")
+else:   
+    print(f"Character '{character}' not found in name.")
+
+# replace a character in the string
+old_character = input("Enter the character to replace: ")
+new_character = input("Enter the new character: ")
+replaced_name = name.replace(old_character, new_character)
+print(f"Replaced name: {replaced_name}")
+
+# and finally here are all string methods that can be used to manipulate strings in python
+
+print(f"help(str) to see all the string methods in python")    
+    
+        
