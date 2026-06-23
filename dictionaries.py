80b41597b6463de1213e7e529f7ad63d173c8478
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-23 18:18:09 +0200

    dictionries

---
+++ dictionaries.py
@@ -0,0 +1,8 @@
+#dictionaries are ordered and changeable
+#dictionaries are a type of collections just like lists, sets and tuples
+#that is the the form of a dictionaries {"key: value","key: value"}
+
+
+
+#these are dictionaries methods
+countries = {"USA: WASHINGTON DC","GERMANY: BERLIN","ALGERIA: ALGIERS"}
