8b0eb450a1e76ae476809a5dcf528e14f6a961e8
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-23 10:19:47 +0200

    number pad

--- number pad.py
+++ number pad.py
@@ -1,4 +1,4 @@
-#we are gonna use a 2D tuple 
+#we are gonna use a 2D tuple to show a number pad
 #just to know there are 2D lists and 2D sets
 num_pad = ((1,2,3),(4,5,6),(7,8,9,),("*",0,"#"))   # thats 2D tuple
 for row in num_pad:
