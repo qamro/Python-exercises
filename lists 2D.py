32f7e082e423b6e1fc1d8e6f5389ddf0c719336d
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-06-25 08:05:55 +0200

    lists 2D 3D

---
+++ lists 2D.py
@@ -0,0 +1,21 @@
+# 2D list
+list_2D = [['A', 'B', 'C', 'D'], ['Q', 'S', 'F', 'G'], ['H', 'J', 'K', 'L']]
+# for accessing an element in our 2D list we use indexing
+print(list_2D[0][0])
+print(list_2D[0][1])
+print(list_2D[2][3])
+print(list_2D[2][0])
+print(list_2D[1][2])
+print(list_2D[0][3])
+
+# 3D list
+list_3D = [[['A', 'B', 'C', 'D'], ['Q', 'S', 'F', 'G'], ['H', 'J', 'K', 'L']],
+            [['P', 'N', 'V', 'X'], ['W', 'M', '0', '9'], ['4', '3', '2', '1']],
+            [['O', 'I', 'U', 'Y'], ['T', 'R', 'E', 'Z'], ['a', 'b', 'c', 'd']]]
+# for accessing an element in our 3D list we use indexing
+print(list_2D[0][0][0])
+print(list_2D[0][1][1])
+print(list_2D[2][2][2])
+print(list_2D[0][2][3])
+print(list_2D[0][1][3])
+print(list_2D[2][0][3])
