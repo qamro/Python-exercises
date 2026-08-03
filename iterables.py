d5d18094390eae0df922c29b56b5c77325b60d6d
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-03 08:38:55 +0200

    terables

--- iterables.py
+++ iterables.py
@@ -1,1 +1,3 @@
-# iterables 
+# iterables is an object or collection that can be iterated over, meaning you can loop through its elements one at a time.
+# In Python, common examples of iterables include lists, tuples, strings, dictionaries, and sets.
+
