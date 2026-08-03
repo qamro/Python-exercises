89ed2c70ce02f31a80fdfca6916f2d906c356635
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-03 08:40:47 +0200

    list iterables

--- iterables.py
+++ iterables.py
@@ -1,3 +1,9 @@
 # iterables is an object or collection that can be iterated over, meaning you can loop through its elements one at a time.
 # In Python, common examples of iterables include lists, tuples, strings, dictionaries, and sets.
 
+numbers = [1, 2, 3, 4, 5] # this is a list, which is an iterable
+for number in numbers:
+    print(number)
+
+for number in reversed(numbers): 
+    print(number)    
