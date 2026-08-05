01a74532e69e8ff79f027e7bd390b23d64e0a7c6
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-05 11:42:29 +0200

    definition of exception

--- exception handling.py
+++ exception handling.py
@@ -1,3 +1,13 @@
 # exception is an event that interrupts the normal flow of a program's execution.
 # It is an error that occurs during the execution of a program
-# examples of exceptions include: ZeroDivisionError, FileNotFoundError, ValueError, TypeError, IndexError, KeyError, etc.
+# Types of exceptions include: ZeroDivisionError, FileNotFoundError, ValueError, TypeError, IndexError, KeyError, etc.
+"""
+the structure of exception handling in python is as follows:
+try:
+    # code that may raise an exception
+except ExceptionType:
+    # code that runs if an exception occurs
+else:
+    # code that runs if no exception occurs
+finally:
+    # code that runs regardless of whether an exception occurs or not"""
