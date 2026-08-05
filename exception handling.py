184a34447a74036b596cc66b390d0a204ea1a220
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-05 11:37:54 +0200

    exceptions

---
+++ exception handling.py
@@ -0,0 +1,3 @@
+# exception is an event that interrupts the normal flow of a program's execution.
+# It is an error that occurs during the execution of a program
+# examples of exceptions include: ZeroDivisionError, FileNotFoundError, ValueError, TypeError, IndexError, KeyError, etc.
