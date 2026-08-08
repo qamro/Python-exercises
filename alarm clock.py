328606b8fd9dce4bca1d5d07e31dbb9b9dc1a3ef
Qamar Eddine Bakhouche <qamrodz@gmail.com>
2026-08-08 16:25:09 +0200

    dates and times

--- alarm clock.py
+++ alarm clock.py
@@ -5,9 +5,10 @@ import pygame
 def set_alarm(alarm_time):
     print(f"Alarm set for: {alarm_time}")
     sound_file = "mixkit-digital-clock-digital-alarm-buzzer-992.wav"  # Replace with the path to your alarm sound file
-    
+    is_running = True
+    while is_running:
+        current
 if __name__ == "__main__":
     alarm_time = input("Enter the alarm time in HH:MM:SS format: ")
     set_alarm(alarm_time)    
     pygame.mixer.init()
-    
